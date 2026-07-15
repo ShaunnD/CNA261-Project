613dd4a2f8a4137b8e8f25ac5d440383d7c4081b
Shaunn <Shaunn.dry@gmail.com>
2026-07-15 12:11:54 +0200

    Add project files.

---
+++ CNA261 Project/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CNA261_Project
+{
+    internal class Program
+    {
+
+        static int GetValidPercentage(string prompt)
+        {
+            int percentage;
+            while (true)
+            {
+                Console.WriteLine(prompt);
+                string input = Console.ReadLine();
+                if (int.TryParse(input, out percentage) && percentage >= 0 && percentage <= 100)
+                {
+                    return percentage; // valid input, return the value
+                }
+                Console.Clear();
+                Console.WriteLine("Please enter a valid percentage between 0 and 100.");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your name:");
+            string name = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("Hello, " + name);
+            Console.WriteLine("Please enter the percentages you received:");
+            int test1 = GetValidPercentage("Test 1");
+            double test1Final = test1 * 0.3;
+            Console.Clear();
+            int test2 = GetValidPercentage("Test 2");
+            double test2Final = test2 * 0.5;
+            Console.Clear();
+            int assignment1 = GetValidPercentage("Assignment 1");
+            double assignment1Final = assignment1 * 0.1;
+            Console.Clear();    
+            int project = GetValidPercentage("Project");
+            double projectFinal = project * 0.1;
+            Console.Clear();    
+            double totalMarks = test1Final + test2Final + assignment1Final + projectFinal;
+            Console.WriteLine(name + " the marks you entered are: Test 1: " + test1 + "%, Test 2: " + test2 + "%, Assignment: " + assignment1 + "% and Project: " + project + "%");
+            Console.WriteLine("Your average is: " + totalMarks + "%");
+            if (totalMarks >= 50)
+            {
+                Console.WriteLine("You have passed the course and qualify for the exam!");
+            }
+            else
+            {
+                Console.WriteLine("You have failed the course.");
+            }
+        }
+
+    }
+}
---
+++ CNA261 Project/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CNA261 Project")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CNA261 Project")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("72e8a222-de4a-4811-8cab-ee5077b84eed")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
